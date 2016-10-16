eaf08b5119f75f0f1e67bc7cd49fbe58670b03d7
emaphis <emaphis85@gmail.com>
2016-10-15 23:11:09 -0400

    more work, this time generators.

--- src/speck/core.clj
+++ src/speck/core.clj
@@ -229,13 +229,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Mutli-spec
 
+;; specs for entities for event objects that share som common fields but
+;; also have type-specifit shapes.
+
 (s/def :event/type keyword?)
-(s/def :event/timestamp integer?)
+(s/def :event/timestamp int?)
 (s/def :search/url string?)
 (s/def :error/message string?)
-(s/def :error/code integer?)
+(s/def :error/code int?)
 
-
+;; We then neeed a mutimethod that defines a dispatch for choosing the  selector:
 
 (defmulti event-type :event/type)
 (defmethod event-type :event/search [_]
@@ -243,28 +246,32 @@
 (defmethod event-type :event/error [_]
   (s/keys :req [:event/type :event/timestamp :error/message :error/code]))
 
+; now our mult-spec:
 (s/def :event/event (s/multi-spec event-type :event/type))
 
 (s/valid? :event/event
   {:event/type :event/search
    :event/timestamp 1463970123000
-   :search/url "http://clojure.org"})
+   :search/url "http://clojure.org"}) ;; true
 
 
 (s/valid? :event/event
   {:event/type :event/error
    :event/timestamp 1463970123000
    :error/message "Invalid host"
-   :error/code 500})
+   :error/code 500}) ;; true
 
 (s/explain :event/event
   {:event/type :event/restart})
 ;; val: {:event/type :event/restart} fails at: [:event/restart] predicate: my.domain/event-type,  no method
+
 (s/explain :event/event
   {:event/type :event/search
    :search/url 200})
-;; val: {:event/type :event/search, :search/url 200} fails at: [:event/search] predicate: [(contains? % :event/timestamp)]
-;; In: [:search/url] val: 200 fails spec: :search/url at: [:event/search :search/url] predicate: 
+;; val: {:event/type :event/search, :search/url 200} fails
+;;  at: [:event/search] predicate: [(contains? % :event/timestamp)]
+;; In: [:search/url] val: 200 fails spec: :search/url
+;;   at: [:event/search :search/url] predicate:
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -275,22 +282,59 @@
 ;; coll-of must be provided a seed collection to use when conforming elements
 ;; - something like [], (), or (sorted-set)
 
-(s/conform (s/coll-of keyword? []) [:a :b :c])
+(s/conform (s/coll-of keyword?) [:a :b :c])
 ;; [:a :b :c]
 
-(s/conform (s/coll-of number? #{}) #{5 10 2})
+(s/conform (s/coll-of keyword?) #{:a :b :c})
+;;#{:c :b :a}
+
+(s/conform (s/coll-of number?) #{5 10 2})
 ;; #{2 5 10}
+
+;; coll-of can be passed a number of optional keyword args:
+;; :kind - a spec that the tested collection mus satisfy
+;; :count - expected cound
+;; :min-count, :max-count - check thte range of collections count
+;; :distinct - all elements distinct?
+;; :into  - one fo [],(),{}, #{}
+
+;; examples:
+(s/def ::vnum3 (s/coll-of number? :kind vector? :count 3 :distinct true :into #{}))
+(s/conform ::vnum3 [1 2 3]) ;; #{1 3 2}
+
+(s/explain ::vnum3 #{1 2 3})
+;; val: #{1 3 2} fails spec: :speck.core/vnum3 predicate: vector?
+
+(s/explain ::vnum3 [1 1 1])
+;; val: [1 1 1] fails spec: :speck.core/vnum3 predicate: distinct?
+
+(s/explain ::vnum3 [1 2 :a])
+;; In: [2] val: :a fails spec: :speck.core/vnum3 predicate: number?
+
 
 ;; another case is a fixed-size positional collection with fields of known type at
 ;; different positions. For that we have tuple
 
-(s/def ::point (s/tuple float? float? float?))
+(s/def ::point (s/tuple double? double? double?))
 (s/conform ::point [1.5 2.5 -0.5])
 ;; [1.5 2.5 -0.5]
 
+;; other choices for point
+
+;; regular expression
+(s/def ::point-2 (s/cat :x double? :y double? :z double?))
+(s/conform ::point-2 [1.5 2.5 -0.5])
+;;{:x 1.5, :y 2.5, :z -0.5}
+
+;; collection
+(s/def ::point-3 (s/coll-of double?))
+(s/conform ::point-3 [1.5 2.5 -0.5])
+;; [1.5 2.5 -0.5]
+
+
 ;; map-of for maps with homogenous key and value predicates.
 
-(s/def ::scores (s/map-of string? integer?))
+(s/def ::scores (s/map-of string? int?))
 (s/conform ::scores {"Sally" 1000, "Joe" 500})
 ;; {"Sally" 1000, "Joe" 500}
 
@@ -299,6 +343,12 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Sequences
 ;; uses  regular expression operators to describe the structure of a sequential data value:
+
+;; cat - concatenation of predicates/patterns
+;; alt - choice among alternative predicates/patterns
+;; * - 0 or more of a predicate pattern
+;; + - 1 or more of a predicate pattern
+;; ? - 0 or 1 of a predicate pattern
 
 ;; an ingredient represented by a vector containing a quantity (number) and a unit (keyword)
 (s/def ::ingedient (s/cat :quantity number? :unit keyword?))
@@ -310,10 +360,10 @@
 
 ;; pass string for unit instead of keyword
 (s/explain ::ingedient [11 "peaches"])
-;; At: [:unit] val: "peaches" fails predicate: keyword?
+;; In: [1] val: "peaches" fails spec: :speck.core/ingedient at: [:unit] predicate: keyword?
 
 (s/explain ::ingedient [2])
-;; At: [:unit] val: () fails predicate: keyword?,  Insufficient input
+;; val: () fails spec: :speck.core/ingedient at: [:unit] predicate: keyword?,  Insufficient input
 
 
 ;;; various occurence operators *, +, and ?:
@@ -324,7 +374,7 @@
 ;; [:a :b :c]
 
 (s/explain ::seq-of-keywords [10 20])
-;; val: 10 fails spec: :speck.core/seq-of-keywords predicate: keyword?
+;; In: [0] val: 10 fails spec: :speck.core/seq-of-keywords predicate: keyword?
 
 (s/def ::odds-then-maybe-even (s/cat :odds (s/+ odd?)
                                      :even (s/? even?)))
@@ -335,10 +385,9 @@
 ;; {:odds [1]}
 
 (s/explain ::odds-then-maybe-even [100])
-;; At: [:odds] val: 100 fails spec: :speck.core/odds-then-maybe-even predicate: odd?
+;; In: [0] val: 100 fails spec: :speck.core/odds-then-maybe-even at: [:odds] predicate: odd?
 
 ;; opts are alternating keyword and booleans
-(defn boolean? [b] (instance? Boolean b))
 (s/def ::opts (s/* (s/cat :opt keyword? :val boolean?)))
 (s/conform ::opts [:silent? false :verbose true])
 ;; [{:opt :silent?, :val false} {:opt :verbose, :val true}]
@@ -413,10 +462,28 @@
   (str (::first-name person) " " (::last-name person)))
 
 (person-name 42)
+;; Java exception:
 ;; Assert failed: (s/valid? :speck.core/person person)
 
 (person-name {::first-name "Elon" ::last-name "Musk" ::email "elon@example.com"})
 ;; "Elon Musk"
+
+
+;; we can also use assert to check conformance to a spec
+
+(defn person-name
+  [person]
+  (let [p (s/assert ::person person)]
+    (str (::first-name p) " " (::last-name p))))
+
+(s/check-asserts true)
+(person-name 100)
+
+;;1. Unhandled clojure.lang.ExceptionInfo
+;;   Spec assertion failed val: 100 fails predicate: map?
+;;   :clojure.spec/failure :assertion-failed
+;;
+ ;;  #:clojure.spec{:problems [{:path [], :pred map?, :val 100, :via [], :in []}], :failure :assertion-failed}
 
 
 ;; call conform and use the return value to destructure the input.
@@ -433,43 +500,39 @@
       (for [{prop :prop [_ val] :val} parsed]
         (set-config (subs prop 1) val)))))
 
+(configure ["-server" "foo" "-verbose" true "-user" "joe"])
+
+;; set server foo
+;; set verbose true
+;; set user joe
+
+(s/conform ::config ["-server" "foo" "-verbose" true "-user" "joe"])
+
+;[{:prop "-server", :val [:s "foo"]}
+; {:prop "-verbose", :val [:b true]}
+; {:prop "-user", :val [:s "joe"]}]
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Spec'ing functions
-;; using fdef
 
+;; using fdef to define specs for a function (arguments, return value, and relationship
+;; between the two.
 
 (defn ranged-rand
   "Returns random integer in range start <= rand < end"
   [start end]
-  (+ start (rand-int (- end start))))
+  (+ start (long (rand (- end start)))))
 
-
+;; now we give this spec:
 (s/fdef ranged-rand
-        :args (s/and (s/cat :start integer? :end integer?)
+        :args (s/and (s/cat :start int? :end int?)
                      #(< (:start %) (:end %)))
-        :ret integer?
+        :ret int?
         :fn (s/and #(>= (:ret %) (-> % :args :start))
                    #(< (:ret %) (-> % :args :end))))
 
-(s/instrument #'ranged-rand)
-
-(ranged-rand 8 5)
-;;   Call to #'speck.core/ranged-rand did not conform to spec: val:
-;;   {:start 8, :end 5} fails at: [:args] predicate: (< (:start %) (:end
-;;   %)) :clojure.spec/args (8 5)
-
-
-;; erroneous version
-(defn ranged-rand   ;; BROKEN!
-  "Returns random integer in range start <= rand < end"
-  [start end]
-  (+ start (rand-int (- start end))))
-
-
-;; Call to  #'spec.examples.guide/ranged-rand did not conform to spec:
-;;val: {:args {:start 5, :end 8}, :ret 3} fails at: [:fn] predicate: (>= (:ret %) (-> % :args :start))
-;;:clojure.spec/args  (5 8)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -485,7 +548,7 @@
         :args (s/cat :x number?)
         :ret  (s/fspec :args (s/cat :y number?)
                        :ret number?)
-        :fn #(= (-> :args :x) ((:ret %) 0)))
+        :fn #(= (-> :args :x) ((:ret %) 0))) ; returns a function
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -494,36 +557,22 @@
 ;; Spec out the clojure.core/declare macro
 
 (s/fdef clojure.core/declare
-  :args (s/cat :names (s/* symbol?))
-  :ret  ::s/any)
+  :args (s/cat :names (s/* simple-symbol?))
+  :ret  any?)
 
 (declare 100)
 
+;; Java exception
 ;;Call to clojure.core/declare did not conform to spec: In: [0] val:
 ;;   (100) fails at: [:args] predicate: (cat :names (* symbol?)), Extra
 ;;   input :clojure.spec/args (100)
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Conformers
-;; Sometimes it will be necessary to automatically modify the conformed value
-;; and this can be accomplished with conformer
-
-(s/def ::name-or-id (s/or :name string?
-                          :id   integer?))
-
-(s/conform ::name-or-id "abc")
-;; [:name "abc"]
-(s/conform ::name-or-id 100)
-;; [:id 100]
-
-;; use a conformer
-(s/def ::name-or-id'
-  (s/and ::name-or-id
-         (s/conformer second)))
-
-(s/conform ::name-or-id' "abc")
-;; "abc"
+;; 1. Unhandled clojure.lang.ExceptionInfo
+;;   Call to clojure.core/declare did not conform to spec: In: [0] val:
+;;   100 fails at: [:args :names] predicate: simple-symbol?
+;;   :clojure.spec/args (100)
+;;
+;;   #:clojure.spec{:problems [{:path [:args :names], :pred simple-symbol?, :val 100, :via [], :in [0]}], :args (100)}
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -537,7 +586,7 @@
 (s/def ::hand (s/* ::card))
 
 (s/def ::name string?)
-(s/def ::score integer?)
+(s/def ::score int?)
 (s/def ::player (s/keys :req [::name ::score ::hand]))
 
 (s/def ::players  (s/* ::player))
@@ -550,15 +599,16 @@
   {::name "Kenny Rogers"
    ::score 100
    ::hand []})
+
 (s/valid? ::player kenny)
 ;; true
 
-;; some bad data:
+;; now some bad data:
 (s/explain ::game
            {::deck deck
             ::players [{::name "Kenny Rogers"
                         ::score 100
-                        ::hand [[2 :banana]]}]})nil
+                        ::hand [[2 :banana]]}]})
 ;; In: [:speck.core/players 0 :speck.core/hand 0 1] val: :banana fails spec:
 ;; :speck.core/card at: [:speck.core/players :speck.core/hand 1] predicate: suit?
 
@@ -578,11 +628,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Generators
 
+;; add this to project.clj to use spec for testing
+;; :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
+
+
+;; require this to run spec generators durring testing
 (require '[clojure.spec.gen :as gen])
 
-(gen/generate (s/gen integer?))
+(gen/generate (s/gen integer?)) ;; -115723691
 
-(gen/generate (s/gen nil?))
+(gen/generate (s/gen nil?)) ;; nil
 
 (gen/sample (s/gen string?))
 ;; ("" "" "u" "0" "" "H7H4" "6f3ZS" "" "wkzBFz7s" "54")
@@ -593,26 +648,172 @@
 
 (gen/sample (s/gen (s/cat :k keyword? :ns (s/+ number?))))
 
+;;((:K -1)
+;; (:!/+z -1)
+;; (:o7/i*3 0.0 1.0 Infinity)
+;; (:UI.*a/EA 0.5 3.5)
+;; (:q02 -4 3.0 1.0 0)
+;; (:uf.z?0.?/F*I86t 1 -0.5 3)
+;; (:uZVp._*+q.T-4/+8_B -1.0 3.0 -0.8125 -3 15)
+;; (:*6.-.N?6?+-E?.+.WK?.*wUW.q!Y/!4*?4r2j -1.0625 33)
+;; (:n7x-?!.c.E4k9g7+a.wzBPFG_6?.Z!!WW/L -1 -0.5 -1 0 -3.0 0 -1.0)
+;; (:-+E.b.-nS-bEI.*6kcZ6/f*N2mL*T -4 2))
+
+;; WOWSERS!
+
 
 ;; generate a random player in out card game
 (gen/generate (s/gen ::player))
 
-;;{:speck.core/name "6r5PkICXTJ4VK8XzU6kgi",
-;; :speck.core/score 11363,
-;; :speck.core/hand ([:king :heart]
-;;                   [:jack :spade]
-;;                   [3 :heart]
-;;                   [8 :club]
-;;                   [9 :heart]
-;;                   [:ace :club]])}
+;;#:speck.core{:name "cet8eRJQ2ybg4s7",
+;;             :score 9436,
+;;             :hand ([3 :spade]
+;;                    [8 :heart]
+;;                    [3 :club]
+;;                    [9 :heart]
+;;                    [:queen :heart]
+;;                    [:jack :heart]
+;;                    [:king :club]
+;;                    [5 :club]
+;;                    [5 :spade]
+;;                    [8 :spade])}
+
 
 ;; generating a whole game
 
 ;; (gen/generate (s/gen ::game))
 
+;; really long game.
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Exercise
+
+(s/exercise (s/cat :k keyword? :ns (s/+ number?)) 5)
+
+;;([(:p -1) {:k :p, :ns [-1]}]
+;; [(:_e/Ff -0.0 -2.0) {:k :_e/Ff, :ns [-0.0 -2.0]}]
+;; [(:f48.j/! 1 -2) {:k :f48.j/!, :ns [1 -2]}]
+;; [(:_9k.p?o/Q 1.0 -1) {:k :_9k.p?o/Q, :ns [1.0 -1]}]
+;; [(:Un!.d?!/jDa*4 2 1 3.0 0 -6) {:k :Un!.d?!/jDa*4, :ns [2 1 3.0 0 -6]}])
+
+(s/exercise (s/or :k keyword? :s string? :n number?) 5)
+
+;;(["" [:s ""]]
+;; [-1 [:n -1]]
+;; [0.5 [:n 0.5]]
+;; ["145" [:s "145"]]
+;; [:*/lPg8! [:k :*/lPg8!]])
+
+
+;; exercise-fn generates sample args for speced functions
+
+(s/exercise-fn `ranged-rand)
+;;([(-1 0) -1]
+;; [(-4 2) 1]
+;; [(-6 0) -1]
+;; [(0 2) 1]
+;; [(-2 6) 4]
+;; [(-2 3) 1]
+;; [(-2 1) 0]
+;; [(-3 17) 3]
+;; [(0 13) 4]
+;; [(-1 1) -1])
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Using s/and Generators
+
+;; a predicate presumes values of a particular type but the spec
+;; does not specify them:
+
+(gen/generate (s/gen even?))
+
+;;1. Unhandled clojure.lang.ExceptionInfo
+;;   Unable to construct gen at: [] for:
+;;   clojure.core$even_QMARK_@353f57d9
+;;   #:clojure.spec{:path [], :form #function[clojure.core/even?], :failure :no-gen}
+
+;; we can use 'and' to create generator for primative predicates that dont have generators
+
+(gen/generate (s/gen (s/and int? even?)))   ; even? acts as a filter
+;; 4
+;; -212800
+
+;; positive numbers of 3:
+(defn divisible-by [n] #(zero? (mod % n)))
+
+(gen/sample (s/gen (s/and int?
+                          #(> % 0)
+                          (divisible-by 3))))
+
+;; (3 18 9 3 3 2340 6 21 321 3)
+
+
+;; consider trying to generate strings that happen to contain the world "hello"
+
+;; hello, are you the one I'm looking for?
+(gen/sample (s/gen (s/and string? #(clojure.string/includes? % "hello"))))
+
+;;1. Unhandled clojure.lang.ExceptionInfo
+;;   Couldn't satisfy such-that predicate after 100 tries.
+;;   {}
+
+;; I guess a million monkeys can't type Shakespeare after all.
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Custom Generators
+
+;; consider a spec with a predicate to specify keywords from a particular namespace:
+
+(s/def ::kws (s/and keyword? #(= (namespace %) "speck.core")))
+(s/valid? ::kws :speck.core/name) ;; true
+(gen/sample (s/gen ::kws))
+;; Couldn't satisfy such-that predicate after 100 tries.
+
+;; A set is a valid predicate spec so we can create one and ask for it’s generator:
+(def kw-gen (s/gen #{:speck.core/name :speck.core/hand :speck.core/id}))
+(gen/sample kw-gen 5)
+
+;;(:speck.core/id
+;; :speck.core/id
+;; :speck.core/name
+;; :speck.core/hand
+;; :speck.core/name)
+
+;; To redefine our spec using this custom generator, use with-gen which takes a spec
+;; and a replacement generator:
+(s/def ::kws (s/with-gen (s/and keyword? #(= (namespace %) "speck.core"))
+               #(s/gen #{:speck.core/name :speck.core/hand :speck.core/id})))
+
+(s/valid? ::kws :speck.core/name) ;; true
+(gen/sample (s/gen ::kws))
+;; (:speck.core/name :speck.core/id :speck.core/name ...)
+
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Instrumentation and Testing
+(s/instrument  #'ranged-rand)
+
+(ranged-rand 8 5)
+;;   Call to #'speck.core/ranged-rand did not conform to spec: val:
+;;   {:start 8, :end 5} fails at: [:args] predicate: (< (:start %) (:end
+;;   %)) :clojure.spec/args (8 5)
+
+
+;; erroneous version
+(defn ranged-rand   ;; BROKEN!
+  "Returns random integer in range start <= rand < end"
+  [start end]
+  (+ start (rand-int (- start end))))
+
+
+;; Call to  #'spec.examples.guide/ranged-rand did not conform to spec:
+;;val: {:args {:start 5, :end 8}, :ret 3} fails at: [:fn] predicate: (>= (:ret %) (-> % :args :start))
+;;:clojure.spec/args  (5 8)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
