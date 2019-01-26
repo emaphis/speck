4df3da2385496c3e366f14b0111b0c22dbbd6aa6
Ed Maphis <emaphis85@gmail.com>
2019-01-26 01:36:15 -0500

    Update fish poem

--- src/speck/fish.clj
+++ src/speck/fish.clj
@@ -1,10 +1,13 @@
 (ns speck.fish
-  (:require [clojure.spec :as s]))
-
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.test.alpha :as stest]))
 
 ;; tutorial:
 ;; <http://gigasquidsoftware.com/blog/2016/05/29/one-fish-spec-fish/#/index>
 
+;;; Specifying the values of the parameters.
+
+;; a notion of fish numbers
 (def fish-numbers {0 "Zero"
                    1 "One"
                    2 "Two"})
@@ -14,33 +17,33 @@
 
 (s/valid? ::fish-number 1) ;; true
 (s/valid? ::fish-number 5) ;; false
+(s/valid? ::fish-number "One") ;; false
 
 
 ;; explain why five is wrong:
 (s/explain ::fish-number 5)
-;; val: 5 fails predicate: (set (keys fish-numbers))
+;; 5 - failed: (set (keys fish-numbers)) spec: :speck.fish/fish-number
 
 
-
-;; color specification
+;; color specification, including Dun
 (s/def ::color #{"Red" "Blue" "Dun"})
 
 ;; Dun rhymes with One  :-)
 
-;;; Specifying the sequence of values
-(s/def ::first-line (s/cat :n1 ::fish-number :n2 ::fish-number :c1 ::color :c2 ::color))
 
+;;; Specifying the sequence of values
+;;  two numbers followed by two colors using `s/cat` for concatenation
+;;  of predicates/patterns.
+(s/def ::first-line (s/cat :n1 ::fish-number :n2 ::fish-number :c1 ::color :c2 ::color))
 
 ;; try a failing spec
 (s/explain ::first-line [1 2 "Red" "Black"])
-;; In: [3] val: "Black" fails spec: :speck.fish/color
-;;  at: [:c2] predicate: #{"Blue" "Dun" "Red"}
+;; "Black" - failed: #{"Blue" "Dun" "Red"} in: [3] at: [:c2] spec: :speck.fish/color
 
 
-;; the second number should be one bigger than the first number. The input
+;; The second number should one be bigger than the first number. The input
 ;; to the function is going to be the map of the destructured tag keys from
-;; the ::first-line
-
+;; the ::first-line.
 (defn one-bigger? [{:keys [n1 n2]}]
   (= n2 (inc n1)))
 
@@ -55,7 +58,6 @@
 ;; test our data
 (s/valid? ::first-line [1 2 "Red" "Blue"]) ;; true
 
-
 ;; the destructured, conformed values
 (s/conform ::first-line [1 2 "Red" "Blue"])
 ;; {:n1 1, :n2 2, :c1 "Red", :c2 "Blue"}
@@ -65,29 +67,27 @@
 (s/valid? ::first-line [2 1 "Red" "Blue"])
 
 (s/explain ::first-line [2 1 "Red" "Blue"])
-;; val: {:n1 2, :n2 1, :c1 "Red", :c2 "Blue"}
-;;  fails predicate: one-bigger?
+;; {:n1 2, :n2 1, :c1 "Red", :c2 "Blue"} - failed: one-bigger? spec: :speck.fish/first-line
 
 
 ;;; Generating test data -- and poetry with specification
 
 (s/exercise ::first-line 5)
-
-;;([(1 2 "Red" "Dun") {:n1 1, :n2 2, :c1 "Red", :c2 "Dun"}]
-;; [(1 2 "Blue" "Dun") {:n1 1, :n2 2, :c1 "Blue", :c2 "Dun"}]
-;; [(1 2 "Blue" "Dun") {:n1 1, :n2 2, :c1 "Blue", :c2 "Dun"}]
-;; [(0 1 "Blue" "Red") {:n1 0, :n2 1, :c1 "Blue", :c2 "Red"}]
-;; [(0 1 "Red" "Dun") {:n1 0, :n2 1, :c1 "Red", :c2 "Dun"}])
+;; ([(1 2 "Dun" "Blue") {:n1 1, :n2 2, :c1 "Dun", :c2 "Blue"}]
+;;  [(0 1 "Dun" "Blue") {:n1 0, :n2 1, :c1 "Dun", :c2 "Blue"}]
+;;  [(0 1 "Blue" "Red") {:n1 0, :n2 1, :c1 "Blue", :c2 "Red"}]
+;;  [(0 1 "Red" "Blue") {:n1 0, :n2 1, :c1 "Red", :c2 "Blue"}]
+;;  [(1 2 "Dun" "Blue") {:n1 1, :n2 2, :c1 "Dun", :c2 "Blue"}])
 
 ;; examples dont' rhyme!
 
 ;; rhyming predicate
-
 (defn fish-number-rhymes-with-color? [{n :n2 c :c2}]
   (or
    (= [n c] [2 "Blue"])
    (= [n c] [1 "Dun"])))
 
+;; add to first-line spec
 (s/def ::first-line (s/and (s/cat :n1 ::fish-number :n2 ::fish-number
                                   :c1 ::color :c2 ::color)
                            one-bigger?
@@ -96,23 +96,21 @@
 
 (s/valid? ::first-line [1 2 "Red" "Blue"]) ;; true
 (s/explain ::first-line [1 2 "Red" "Dun"]) ;; nil
-;; val: {:n1 1, :n2 2, :c1 "Red", :c2 "Dun"}
-;;  fails predicate: fish-number-rhymes-with-color?
+;; {:n1 1, :n2 2, :c1 "Red", :c2 "Dun"}
+;; - failed: fish-number-rhymes-with-color? spec: :speck.fish/first-line
 
-
+;; data generation with new spec.
 (s/exercise ::first-line)
-
-;;([(1 2 "Red" "Blue") {:n1 1, :n2 2, :c1 "Red", :c2 "Blue"}]
-;; [(1 2 "Dun" "Blue") {:n1 1, :n2 2, :c1 "Dun", :c2 "Blue"}]
-;; [(1 2 "Dun" "Blue") {:n1 1, :n2 2, :c1 "Dun", :c2 "Blue"}]
-;; [(0 1 "Blue" "Dun") {:n1 0, :n2 1, :c1 "Blue", :c2 "Dun"}]
-;; [(0 1 "Blue" "Dun") {:n1 0, :n2 1, :c1 "Blue", :c2 "Dun"}]
-;; [(1 2 "Dun" "Blue") {:n1 1, :n2 2, :c1 "Dun", :c2 "Blue"}]
-;; [(0 1 "Red" "Dun") {:n1 0, :n2 1, :c1 "Red", :c2 "Dun"}]
-;; [(1 2 "Red" "Blue") {:n1 1, :n2 2, :c1 "Red", :c2 "Blue"}]
-;; [(1 2 "Red" "Blue") {:n1 1, :n2 2, :c1 "Red", :c2 "Blue"}]
-;; [(0 1 "Red" "Dun") {:n1 0, :n2 1, :c1 "Red", :c2 "Dun"}])
-
+;; ([(1 2 "Red" "Blue") {:n1 1, :n2 2, :c1 "Red", :c2 "Blue"}]
+;;  [(0 1 "Blue" "Dun") {:n1 0, :n2 1, :c1 "Blue", :c2 "Dun"}]
+;;  [(0 1 "Red" "Dun") {:n1 0, :n2 1, :c1 "Red", :c2 "Dun"}]
+;;  [(0 1 "Blue" "Dun") {:n1 0, :n2 1, :c1 "Blue", :c2 "Dun"}]
+;;  [(0 1 "Blue" "Dun") {:n1 0, :n2 1, :c1 "Blue", :c2 "Dun"}]
+;;  [(1 2 "Red" "Blue") {:n1 1, :n2 2, :c1 "Red", :c2 "Blue"}]
+;;  [(1 2 "Dun" "Blue") {:n1 1, :n2 2, :c1 "Dun", :c2 "Blue"}]
+;;  [(0 1 "Red" "Dun") {:n1 0, :n2 1, :c1 "Red", :c2 "Dun"}]
+;;  [(1 2 "Red" "Blue") {:n1 1, :n2 2, :c1 "Red", :c2 "Blue"}]
+;;  [(1 2 "Red" "Blue") {:n1 1, :n2 2, :c1 "Red", :c2 "Blue"}])
 
 
 ;; Using spec with functions
@@ -130,33 +128,23 @@
 ;; specify that the args for this function be validated with ::first-line
 ;; and the return value is a string.
 (s/fdef fish-line
-        :args ::first-line
-        :ret  string?)
-
+  :args ::first-line
+  :ret string?)
 
 ;; turn on the instrumentation of the validation for functions and see what happens.
 
-(s/instrument #'fish-line)
+(stest/instrument `fish-line)
 
 (fish-line 1 2 "Red" "Blue")
 ;; "One fish. Two fish. Red fish. Blue fish."
 
-;; bad data
+;; bad data?
 (fish-line 2 1 "Red" "Blue")
-;; Call to #'speck.fish/fish-line did not conform to spec: val: {:n1
-;;   2, :n2 1, :c1 "Red", :c2 "Blue"} fails at: [:args] predicate:
-;;   one-bigger?  :clojure.spec/args (2 1 "Red" "Blue")
+;; Spec: #object[clojure.spec.alpha$and_spec_impl$reify__2183 0xe111046
+;;               "clojure.spec.alpha$and_spec_impl$reify__2183@e111046"]
+;; Value: (2 1 "Red" "Blue")
 
-;;{:clojure.spec/problems
-;; {[:args]
-;;  {:pred one-bigger?,
-;;   :val {:n1 2, :n2 1, :c1 "Red", :c2 "Blue"},
-;;   :via [],
-;;   :in []}},
-;; :clojure.spec/args (2 1 "Red" "Blue")}
+;; Problems:
 
-
-(defn add3
-  "Add 3 to a given number"
-  [num]
-  (+ 3 num))
+;; val: {:n1 2, :n2 1, :c1 "Red", :c2 "Blue"}
+;; failed: one-bigger?
